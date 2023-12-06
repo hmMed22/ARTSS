7488ae467390eaa371f5984571d9d1b17b13c632
Hajar Moradmand <44647047+htarghi@users.noreply.github.com>
2023-12-06 12:02:25 -0600

    Update Reorient.py

--- ARTSS_Package/Reorient.py
+++ ARTSS_Package/Reorient.py
@@ -33,13 +33,18 @@ class Reorient:
         return rotated_image, predicted_orientation
 
     def rotate_image(self, image, angle):
-        if angle == 90:
+        # Calculate the necessary rotation to bring the image to the desired 90-degree orientation (fingers upward)
+        rotation_required = 90 - angle  # Calculate the rotation required from the predicted angle
+
+        # Rotate the image to achieve the desired orientation (90 degrees, fingers upward)
+        if rotation_required == 90:
             rotated_image = cv2.rotate(image, cv2.ROTATE_90_CLOCKWISE)
-        elif angle == 180:
+        elif rotation_required == 180:
             rotated_image = cv2.rotate(image, cv2.ROTATE_180)
-        elif angle == 270:
+        elif rotation_required == 270:
             rotated_image = cv2.rotate(image, cv2.ROTATE_90_COUNTERCLOCKWISE)
         else:
-            rotated_image = image  # No rotation needed for 0 degrees
+            rotated_image = image  # No rotation needed for 0 degrees or multiples of 360
+
         return rotated_image
 
